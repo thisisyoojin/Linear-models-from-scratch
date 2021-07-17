0b69ac8f0b087805bc27ff5624e2d51b18f1ee7f
thisisyoojin <thisisyoojin@gmail.com>
2021-07-17 19:01:11 +0100

    Implements train_test_split function

--- model_selection/train_test_split.py
+++ model_selection/train_test_split.py
@@ -1,6 +1,28 @@
+import numpy as np
 
-def train_test_split():
+def train_test_split(X, y, train_size=None, test_size=0.2):
     """
     Split arrays or matrices into random train and test subsets
+
+    INPUT
+    X: Numpy array of features - Matrix
+    y: Numpy array of label - Vector
     """
-    pass
+
+    if train_size:
+        test_size = 1 - train_size
+
+    idx = 0
+    test_data_count = len(X) * test_size
+    X_test = np.empty(X.shape[1])
+    y_test = np.empty(1)
+
+    while idx < test_data_count:
+        generated_num = np.random.randint(0, len(X))
+        X_test = np.vstack((X_test, X[generated_num]))
+        y_test = np.vstack((y_test, y[generated_num]))
+        np.delete(X, generated_num, axis=0)
+        np.delete(y, generated_num, axis=0)
+        idx += 1
+
+    return X, X_test, y, y_test
