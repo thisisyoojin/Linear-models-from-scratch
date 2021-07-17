7553704259ce13cba39456186ab3ef07b8881c7b
thisisyoojin <thisisyoojin@gmail.com>
2021-07-17 19:44:30 +0100

    Adds calculate_gradient function

--- linear_model/LinearRegression.py
+++ linear_model/LinearRegression.py
@@ -21,7 +21,13 @@ class LinearRegression:
         """
         Fit the model according to the given training data
         """
+        # Initialise the parameters for model
+        n_features = X.shape[1]
+        self.w = np.random.randn(n_features)
+        self.b = np.random.randn()
+        
         pass
+
 
     def predict(self, X):
         """
@@ -32,18 +38,31 @@ class LinearRegression:
         return np.matmul(X, self.w) + self.b
 
 
-    def calculate_loss(self):
+    def calculate_loss(self, X, y):
         """
-        Optimisor for the linear regression
+        MSE(Mean Squared Error)
         """
         pass
 
 
-    def calculate_gradient(self):
+    def calculate_gradient(self, X, y):
         """
         Calculate gradient for the current parameters
         """
-        pass
+        # predict the value with a model
+        y_pred = self.predict(X)
+
+        # calculate the gradient for weights(coefficient)
+        grad_individuals = []
+        for idx in range(len(X)):
+            grad = 2 * (y_pred[idx] - y[idx]) * X[idx]
+            grad_individuals.append(grad)
+        grad_w = np.mean(grad_individuals, axis=0)
+        
+        # calculate the gradient for bias
+        grad_b = 2 * np.mean(y_pred - y)
+
+        return grad_w, grad_b
 
 
     def score(self):
