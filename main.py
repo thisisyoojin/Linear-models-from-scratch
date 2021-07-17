729b194804594716eba46583e77b67369f7190bf
thisisyoojin <thisisyoojin@gmail.com>
2021-07-17 19:14:40 +0100

    Defines a regression function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from linear_model import LinearRegression
 from sklearn.datasets import load_boston
+from model_selection import train_test_split
+from linear_model import LinearRegression
+
+def regression():
+    X, y = load_boston(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train)
+    regressor = LinearRegression()
 
 
 if __name__ == "__main__":
-    regressor = LinearRegression()
+    regression()
+    
