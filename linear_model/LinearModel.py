2205c283d279b6f64b35e2813d0bb7f6b2f6c6f2
thisisyoojin <thisisyoojin@gmail.com>
2021-07-18 21:41:35 +0100

    Reconstructs a structure(subclass)

--- linear_model/LinearModel.py
+++ linear_model/LinearModel.py
@@ -1,3 +1,55 @@
+import numpy as np
 
 class LinearModel:
-    pass
+
+    def __init__(self, learning_rate=0.01, batch_size=16):
+        """
+        Initialise linear regression model
+        """
+        self.w = None
+        self.b = None
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+
+
+    @property
+    def coef_(self):
+        """
+        Get weight(coefficient) for this estimator
+        """
+        return self.w
+    
+    @property
+    def intercept_(self):
+        """
+        Get bias(intercept) for this estimator
+        """
+        return self.b
+
+
+    def predict(self, X):
+        """
+        Predict the values
+        """
+        if self.w is None:
+            raise Exception("You should fit a model first.")
+        return np.matmul(X, self.w) + self.b        
+
+
+    def calculate_gradient(self, X, y, y_pred):
+        """
+        Calculate gradient for the current parameters
+        """
+        # predict the value with a model
+
+        # calculate the gradient for weights(coefficient)
+        grad_individuals = []
+        for idx in range(len(X)):
+            grad = 2 * (y_pred[idx] - y[idx]) * X[idx]
+            grad_individuals.append(grad)
+        grad_w = np.mean(grad_individuals, axis=0)
+        
+        # calculate the gradient for bias
+        grad_b = 2 * np.mean(y_pred - y, axis=0)
+
+        return grad_w, grad_b
--- linear_model/LinearRegression.py
+++ linear_model/LinearRegression.py
@@ -1,31 +1,15 @@
+from linear_model.LinearModel import LinearModel
 import numpy as np
 import matplotlib.pyplot as plt
 from .Batchifier import Batchifier
 
-class LinearRegression:
+class LinearRegression(LinearModel):
     
     def __init__(self, learning_rate=0.01, batch_size=16):
         """
         Initialise linear regression model
         """
-        self.w = None
-        self.b = None
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
-
-    @property
-    def coef_(self):
-        """
-        Get weight(coefficient) for this estimator
-        """
-        return self.w
-    
-    @property
-    def intercept_(self):
-        """
-        Get bias(intercept) for this estimator
-        """
-        return self.b
+        super().__init__(learning_rate=learning_rate, batch_size=batch_size)
 
 
     def fit(self, X_train, y_train, X_val=None, y_val=None, epochs=50, learning_rate=None, batch_size=None, draw=False):
@@ -96,14 +80,6 @@ class LinearRegression:
         return train_losses, val_losses
 
 
-    def predict(self, X):
-        """
-        Predict the values
-        """
-        if self.w is None:
-            raise Exception("You should fit a model first.")
-        return np.matmul(X, self.w) + self.b
-
 
     def calculate_loss(self, y, y_pred):
         """
@@ -111,24 +87,6 @@ class LinearRegression:
         """
         return np.mean((y_pred - y)**2)
 
-
-    def calculate_gradient(self, X, y, y_pred):
-        """
-        Calculate gradient for the current parameters
-        """
-        # predict the value with a model
-
-        # calculate the gradient for weights(coefficient)
-        grad_individuals = []
-        for idx in range(len(X)):
-            grad = 2 * (y_pred[idx] - y[idx]) * X[idx]
-            grad_individuals.append(grad)
-        grad_w = np.mean(grad_individuals, axis=0)
-        
-        # calculate the gradient for bias
-        grad_b = 2 * np.mean(y_pred - y, axis=0)
-
-        return grad_w, grad_b
 
 
     def score(self, X, y_true):
